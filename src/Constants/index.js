a5b6dad520c73ecd8a2ec78b17146f68254912fb
Nguyen Thanh Trung <trungntgcd17186@fpt.edu.vn>
2022-02-24 13:52:25 +0700

    fix array src img background

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,3 +1,6 @@
+export const defaultSrcImg =
+  "https://tophinhanhdep.com/wp-content/uploads/2021/10/1920X1080-HD-Beach-Desktop-Wallpapers.jpg";
+
 export const srcImgData = [
   "https://tophinhanhdep.com/wp-content/uploads/2021/10/1920X1080-HD-Beach-Desktop-Wallpapers.jpg",
   "https://i.ytimg.com/vi/iA9WOk654kc/maxresdefault.jpg",
@@ -5,6 +8,3 @@ export const srcImgData = [
   "http://3.bp.blogspot.com/-xNHvv4ojNYU/UxLmhstnhLI/AAAAAAAAIQY/n8KRpyx5gz0/s1600/Dragon-Ball-SonGoku-Desktop-Wallpaper-full-HD-Infographic-BLOG-18.jpg",
   "https://phunugioi.com/wp-content/uploads/2020/02/background-dep-chat-luong-cao.jpg",
 ];
-
-export const defaultSrcImg =
-  "https://tophinhanhdep.com/wp-content/uploads/2021/10/1920X1080-HD-Beach-Desktop-Wallpapers.jpg";
